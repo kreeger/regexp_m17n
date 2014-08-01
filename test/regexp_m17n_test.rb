5fa21aeb8a56d0c2940e89b5f8e3287568d1bba1
Ben Kreeger <benjaminkreeger@gmail.com>
2014-08-01 08:26:36 -0500

    Remove iconv and just skip non UTF-8-mappable conversions.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -2,16 +2,11 @@
 require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
-require 'iconv'
-
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      begin
-        encoded = '.'.encode(enc)
-      rescue Encoding::ConverterNotFoundError
-        encoded = Iconv.conv('.'.encoding.to_s, enc.to_s, '.')
-      end
+      next if %w(ISO-2022-JP UTF-7).include?(enc.to_s)
+      encoded = '.'.encode(enc)
       assert(RegexpM17N.non_empty?(encoded))
     end
   end
