f66d604856f99852c3649b6f8e96c03ca21a9841
Ben Kreeger <benjaminkreeger@gmail.com>
2014-08-01 08:30:09 -0500

    Also skip ISO-2022-JP-2.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,7 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      next if %w(ISO-2022-JP UTF-7).include?(enc.to_s)
+      next if %w(ISO-2022-JP ISO-2022-JP-2 UTF-7).include?(enc.to_s)
       encoded = '.'.encode(enc)
       assert(RegexpM17N.non_empty?(encoded))
     end
