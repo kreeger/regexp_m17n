cc0f852fbaf6586f468a8402d184d4ac2cc541fd
Ben Kreeger <benjaminkreeger@gmail.com>
2014-08-01 08:07:27 -0500

    Add potential solution to regex match issue.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -2,10 +2,17 @@
 require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
+require 'iconv'
+
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      begin
+        encoded = '.'.encode(enc)
+      rescue Encoding::ConverterNotFoundError
+        encoded = Iconv.conv('.'.encoding.to_s, enc.to_s, '.')
+      end
+      assert(RegexpM17N.non_empty?(encoded))
     end
   end
 end
